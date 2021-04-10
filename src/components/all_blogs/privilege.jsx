8d235dd4ae639561ee489ab7807bf6c9154234be
Neeraj Chouhan <48256974+NeerajChouhan7@users.noreply.github.com>
2021-04-10 23:53:08 +0530

    Update privilege.jsx

--- src/components/all_blogs/privilege.jsx
+++ src/components/all_blogs/privilege.jsx
@@ -23,22 +23,22 @@ We all know what privilege means in the materialistic sense. But what does it me
 
 <br />
 <br />
-<h4 className='strong'>Leveraging privilege</h4>
+<h4 className='strong' style={"fontWeight: bold"}>Leveraging privilege</h4>
 Most people who make it, leverage privilege. It is a moat. At a young age, your source of privilege has the authority to make decisions. But with experience and awareness, you come to realize the effortless benefits and edges you had over others. These advantages could have a significant impact on future events. I mean it surely does, believing that free will does not exist. The family you were born in will affect your life, no doubt about that. But how do you leverage the resources you have and help others and yourself is up to you. Which affects your well-being in life. 
 
 <br />
 <br />
-<h4 className='strong'>Bias towards privilege</h4>
+<h4 className='strong' style={"fontWeight: bold"}>Bias towards privilege</h4>
 Privileged people knowingly or unknowingly have the bias of assumption. Assuming that everyone is or must be equally privileged as I am. This is unexplored territory they want to explore. This is a curiosity gap that they want to fill in. The preconceived notion of equal privilege lies inherently. It could be because of a lack of past shreds of evidence or lack of data to refer to. Majorly this bias happens towards money more than anything else. It is more evolutionary, I believe. Because in tribes, financially everyone was equally privileged. Obviously, some people had dominance and authority but in terms of materialistic ownership, they all were the same. Which is now what we are seeing in the capitalistic world.
 
 <br />
 <br />
-<h4 className='strong'>Going down spiral because of lack of it</h4>
+<h4 className='strong' style={"fontWeight: bold"}>Going down spiral because of lack of it</h4>
 When you are aware that you do not have that luxury, security, or comfort, for the lack of a better word. It is quite possible that you will fall into chaos, the unexplored territory. Luckily, most people are not aware or do not think about it until considerably old age. And those are the luckiest ones I would say. But those who do, think that why I did not have that materialistic belonging when I was young, or maybe right now? Was I not good enough? What did I do? And It is completely fine to get all these questions because at the end of the day your brain has to make sense of the situation. You have to explore that territory, you have brought order to that chaotic part. The conclusion that you could or should come down to is that it was not my fault, for the lack of a better word. My privilege was not in my hands. It was out of my control and it is not possible on earth that I could have controlled it.
 
 
 <br /><br />
-<h4 className='strong'>Excess privilege</h4>
+<h4 className='strong' style={"fontWeight: bold"}>Excess privilege</h4>
 Providing excess privilege to a human is not good from a psychological standpoint. It makes it painfully hard for the provider to handle the given privilege, taking into account that they care about the person who is accessing it. Taking an example of a parent and child here. If the parents have excess access to resources which is here money. How they could refuse their child to buy something or help with anything. Now, here if parents are smart enough, they understand that constantly providing children what they want begets two things. First, they devalue the resource. Second, the desires continue to grow. And it is proven that there is no limit to human desires. Hence, it becomes really hard for parents with access to abundance resources to bring their child in the best way they have in their mind. To quote Jordan Peterson here, "Excess privilege creates spoiled children because they get everything by doing nothing and that won't work in real-world"
 
 <br /><br />
