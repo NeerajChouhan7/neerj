133a23448066a4693f8337b00f85ddb086448eb3
Neeraj Chouhan <48256974+NeerajChouhan7@users.noreply.github.com>
2021-06-08 10:34:09 +0530

    Update work.jsx

--- src/components/work.jsx
+++ src/components/work.jsx
@@ -12,11 +12,21 @@ function Work() {
                 </ol>
             </nav>
            <h6>Currently...</h6>
-           <p>Product Management Intern at <a>Upraised</a></p>
-           <p>Writing newsletter on <a href='https://neerajc.substack.com/'>Substack</a>. Just read it. Don't sub, but read.</p>
-           <p>Running a newsletter on psychology with ~300 members. If you want to be part of it, click <a href='https://api.whatsapp.com/send/?phone=917247796922&text=Hey,+I+got+to+know+about+Psych.+I+would+love+to+be+part+of+it!&app_absent=0'>here</a></p>
-           <p>Built several projects as a freelancer and a developer</p>
-           <p>More about past work exp. <a href='https://www.linkedin.com/in/neeraj-chouhan-profile/'>here</a></p>
+           <li>
+             <p>Product Management Intern at <a>Upraised</a></p>
+           </li>
+           <li>
+            <p>Writing newsletter on <a href='https://neerajc.substack.com/'>Substack</a>. Just read it. Don't sub, but read.</p>
+           </li>
+           <li>
+            <p>Running a newsletter on psychology with ~300 members. If you want to be part of it, click <a href='https://api.whatsapp.com/send/?phone=917247796922&text=Hey,+I+got+to+know+about+Psych.+I+would+love+to+be+part+of+it!&app_absent=0'>here</a></p>
+           </li>
+           <li>
+            <p>Built several projects as a freelancer and a developer</p>
+           </li>
+           <li>
+            <p>More about past work exp. <a href='https://www.linkedin.com/in/neeraj-chouhan-profile/'>here</a></p>
+           </li>
         </div>
     )
 }
