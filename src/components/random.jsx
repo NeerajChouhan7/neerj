3e4c9dc723de4a9f476c75fca37ab6b6e8280027
Neeraj Chouhan <48256974+NeerajChouhan7@users.noreply.github.com>
2021-07-17 16:51:18 +0530

    Update random.jsx

--- src/components/random.jsx
+++ src/components/random.jsx
@@ -3,14 +3,14 @@ import { Link } from 'react-router-dom';
 
 export default function random(){
     return(
-        <div className='container mt-5'>
+        <div className='container mt-5 start text-start'>
              <nav aria-label="breadcrumb">
                 <ol className="breadcrumb">
                 <li className="breadcrumb-item"><Link to='/'>Home</Link></li>
                     <li className="breadcrumb-item active" aria-current="page">Contact</li>
                 </ol>
             </nav>
-            <ul>
+            <ul className='start text-start'>
                 <li>
                     <h6><a target='_blank' href='https://twitter.com/_neerajchouhan'>Twitter</a></h6>
                 </li>
