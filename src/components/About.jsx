d5deb7b1b5c4aed21fba596c5d9264088c21a42c
Neeraj Chouhan <48256974+NeerajChouhan7@users.noreply.github.com>
2021-09-11 15:42:44 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -16,8 +16,8 @@ function About() {
             <p>A nomad genz, currently based out of India.</p>
             <p>It has been quite difficult for me to list down my interests.</p>
             <p>But some of the fields that I have been interested from quite a long time are tech, art, psychology and writing. In the same order of magnitude.</p>
-            <p>In the near future, I will be studying one of the above mentioned fields in usa.</p>
-            <p>Running few things on the internet, here and there. More about them <Link to='/work'>here</Link>.</p>
+            <p>In the near future, I will be studying one of the above mentioned fields abroad.</p>
+            <p>Running few things on the internet, more about them <Link to='/work'>here</Link>.</p>
             <p>p.s. feel free to reach out, if you are working on some cool stuff, need help with anything or just intellectually curious. Here's my <Link to='/random'>contact</Link></p>
             
         </div>
