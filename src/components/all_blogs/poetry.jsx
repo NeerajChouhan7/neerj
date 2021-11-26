ff3777da3d5d8d1e06948dd5a0c57f6fb281caf4
Neeraj Chouhan <48256974+NeerajChouhan7@users.noreply.github.com>
2021-11-26 06:06:42 +0530

    Added all poetries

--- src/components/all_blogs/poetry.jsx
+++ src/components/all_blogs/poetry.jsx
@@ -11,7 +11,8 @@ return(
                 </ol>
             </nav>
             <div>
-                <p>lol idk why I shared this with you...Oh Captain My Captain!</p>
+                <p>lol idk why I shared this with you...</p>
+                <p>didn't named any of the poem cus ik the meanings are going to keep changing for me and you too :)</p>
                 <br/>
                 <br/>
                 <h6>1.</h6>
@@ -20,11 +21,11 @@ return(
                 I wish I was dead and cold,<br/>
 seeking validation from my demons<br/>
 tried, but could no more hold<br/>
-going down spiral my head uphold<br/>
+going down spiral my head uphold<br/><br/>
 Then, maybe I could have embraced<br/>
 sleepless nights in my bed,<br/>
 love and hate, only in my head,<br/>
-uninterested talks with my friend.<br/>
+uninterested talks with my friend.<br/><br/>
 All went down in drain,<br/>
 when I lost to myself<br/>
 and my pain<br/>
@@ -41,7 +42,7 @@ Surely, it was me, not you.
 Staring in the abyss,
 <br/>
 not to find myself but you
-<br/>
+<br/><br/>
 Came back from darkness,
 <br/>
 hoping for ecstacy to come and find me.
@@ -49,14 +50,14 @@ hoping for ecstacy to come and find me.
 Letting go could be an act of kindness,<br/>
 
 maybe for you, but not for me. <br/>
-
+<br/>
 After, essence of life was lost,<br/>
 
 thought, I could find that myself.<br/>
 
 Turns out, essence was you<br/>
 </p>
-<br/>
+                <br/>
                 <br/>
                 <h6>3.</h6>
                 <br/>
@@ -64,7 +65,7 @@ Turns out, essence was you<br/>
                 <br/>
 don't stop, bind or catch me
 <br/>
-let me fly.
+let me fly.<br/>
 <br/>
 Without any awareness, only ignorance
 <br/>
@@ -73,7 +74,7 @@ you are binding me in invisible chain.
 Dare you do that to my playfulness,
 <br/>
 hoping for life to come and find me in my pain.
-<br/>
+<br/><br/>
 Not leap of ignorance but of faith.
 <br/>
 Trust me, and I will try,
@@ -81,6 +82,430 @@ Trust me, and I will try,
 not to disappoint you or make you cry
 <br/>
 But at least let me try
+<br/>
+                <br/>
+                <h6>4.</h6>
+                <br/>
+                <p>
+                So for now, let's all enjoy this view,
+<br/>
+persevering in the hardship,
+<br/>
+pausing not to reflect of thy but on you,
+<br/>
+treating your time not as worship
+
+                </p>
+                
+                <br/>
+                <br/>
+                <h6>5.</h6>
+                <br/>
+                <p>
+                That corner of my mind, just to hide,
+                <br/>
+where my all fears reside.
+<br/>
+Trying to look them in the eyes,<br/>
+
+hiding from emotions in the tide.<br/>
+<br/>
+You will have to look,<br/>
+
+you can't hide for your life<br/>
+
+spine straight, shoulders unhook,<br/>
+
+if you won't, it will left you shook.<br/><br/>
+meet eye to eye to your fear<br/>
+talk to it to know it,
+<br/>
+
+understand it,
+<br/>
+
+kill it
+                </p>
+                <br/>
+                <br/>
+                <h6>6.</h6>
+                <br/>
+                <p>
+                Applaud my friends,
+                <br/>
+the comedy is over.
+<br/>
+End of my destined path,
+<br/>
+the tragedy is over.
+<br/>
+<br/>
+Start laughing and stop crying, 
+<br/>
+it was a circus, but we all were joker.
+<br/>
+With constant suffering and trying,
+<br/>
+remorse behind the laugh, residing.
+<br/>
+<br/>
+Remorse for seeking validation,
+<br/>
+not being with loved one, not following your passion.</p><br/>
+                <br/>
+                <h6>7.</h6>
+                <br/>
+                <p>
+                Gusse se bhara hai ye ateet mera, chahu magar bhul na pau.
+                <br/>
+kehta hai wo utaar de kaagaz pe saara,
+<br/>
+wo kya jaane, chahta me bhi hu par himmat na juta pau.
+<br/>
+<br/>
+Khuda ko bol aake puch le mere se meri raza kya hai.
+<br/>
+Shayad me jawab dene ki takleef me khud bhi jaan jau.
+<br/>
+Khade to me or khuda the waha,
+<br/>
+par baat dono ka ahankaar raha hai.</p><br/>
+                <br/>
+                <h6>8.</h6>
+                <br/>
+                <p>
+                To not let go of the grudges that I hold to this day,
+                <br/>
+But to the happy memories that I could never let go of. 
+<br/><br/>
+To make living hell on earth, just tell me, the one
+<br/>
+Suffering are my friends, I'm not there.
+<br/>
+To help them, console them or die with them,
+<br/>
+inserting bad moments, in the loopholes of happy ones.</p><br/>
+                <br/>
+                <h6>9.</h6>
+                <br/>
+                <p>
+                Most pretends to be friends, others got some already.
+                <br/>
+You, my friend, become your own.
+<br/>
+Path is lonelier than you imagined,
+<br/>
+waiting for that one, to make me forget other eleven.
+<br/><br/>
+But don't worry it's not test for you,
+<br/>
+but for those who stays.
+<br/>
+Look over your shoulder in hardship,
+<br/>
+who is standing their, has pass the test.
+<br/>
+If no one has, give it time, be patient.
+                    </p><br/>
+                <br/>
+                <h6>10.</h6>
+                <br/>
+                <p>
+                Our eyes met.
+                <br/>
+Everyday my eyes used to go to meet her.
+<br/>
+But i didn't, yet.
+<br/><br/>
+Only for a single glance,
+<br/>
+I ride, to let fate take the chance.
+<br/>
+Everyday there was only one moment,
+<br/>
+but it made whole life, a better dance.
+<br/><br/>
+She smiled rarely with her eyes, 
+<br/>
+but her lips were suffice.
+<br/>
+They told me everything,
+<br/>
+she was afraid to compromise.
+<br/><br/>
+Eyes, chico, they never lie,
+<br/>
+how could they when heart bona fide.
+<br/>
+her hairs, in my finger, tie.
+<br/>
+to her eyes i still cannot lie.
+                    </p><br/>
+                <br/>
+                <h6>11.</h6>
+                <br/>
+                <p>
+                Would you never know that that was it? 
+                <br/>
+wandering around finding your why
+<br/>
+just to realize that you lost the shit
+<br/>
+was that you or them, but you both lost it.
+<br/><br/>
+Will you stop questioning they would be as sad as you.
+<br/>
+would they be thinking about you, as much as you,
+<br/>
+will you need them, as you need you,
+<br/>
+guess you never know, or would you?
+<br/><br/>
+Wish someone told me, us, that was the last one,
+<br/>
+to make it as cheerful as one could make,
+<br/>
+no matter how long it take,
+<br/>
+we would have felt like it took one.
+<br/><br/>
+At the end, pondering over what it's worth anyway,
+<br/>
+you've gone too far to make it work,
+<br/><br/>
+memories are being too stubborn to let go,
+<br/>
+nights are sleepless to talk anyone not to,
+<br/>
+would you have let me go,
+<br/>
+if you had anyone else to talk to.
+<br/><br/>
+wearing your woolen sweater,
+<br/>
+you said it looked better, did you.
+<br/>
+sore eyes, brighter than the blue sky 
+<br/><br/>
+we could have build a castle,
+<br/>
+of our dreams, 
+<br/>
+make sure not of sand to avoid drizzle,
+<br/>
+as a team.</p><br/>
+                <br/>
+                <h6>12.</h6>
+                <br/>
+                <p>
+                with moving towards death,
+                <br/>
+at each point of short but big life,
+<br/>
+we notice seasons differently. 
+<br/><br/>
+Some emotions get attached to those seasons,
+<br/>
+some seasons get attached to our hearts. 
+<br/>
+With body adapting to those seasons,
+<br/>
+our heart finds it difficult to do so. Sometimes it doesn't.
+<br/><br/>
+walking, talking, behavior, everything seems seperated by seasons.
+                </p>
+                <br/>
+                <h6>13.</h6>
+                <br/>
+                <p>
+                I'm lying to myself that I can see,
+                <br/>
+to file myself and make a plea
+<br/>
+I will plead guilty
+<br/>
+but with setting me free.
+<br/><br/>
+With the lie of watching myself,
+<br/>
+i wandered every corner of my city.
+<br/>
+to, as they say, know thyself.
+<br/>
+came back with scar on my face by followers of a deity
+<br/><br/>
+Did they knew themselves? I wondered
+<br/>
+Well, i can believe that and i did.
+<br/>
+In thunder, that's only thing they remembered.
+<br/>
+not caring about how many were murdered.
+<br/><br/>
+Again went back wandering around streets,
+<br/>
+knowing destination was at my place.
+<br/>
+I still wandered leaving every trace,,
+<br/>
+no one knew my face,
+<br/>
+I was a mess.
+<br/><br/>
+Maybe it was the fog or lights blinding
+<br/>
+with a torch but battery abiding.
+<br/>
+stepped into fogged faking courage
+<br/>
+just to face my face and a stone colliding.
+                </p>
+                <br/>
+                <h6>14.</h6>
+                <br/>
+                <p>
+                Our eyes met.
+                <br/>
+Everyday my eyes used to go to meet her.
+<br/>
+But i didn't, yet.
+<br/><br/>
+Only for a single glance,
+<br/>
+I ride, to let fate take the chance.
+<br/>
+Everyday there was only one moment,
+<br/>
+but it made whole life, a better dance.
+<br/><br/>
+She smiled rarely with her eyes, 
+<br/>
+but her lips were suffice.
+<br/>
+They told me everything,
+<br/>
+she was afraid to compromise.
+<br/><br/>
+Eyes, chico, they never lie,
+<br/>
+how could they when heart bona fide.
+<br/>
+her hairs, in my finger, tie.
+<br/>
+to her eyes i still cannot lie.
+                </p>
+     <br/>           <br/>
+                <h6>15.</h6>
+                <br/>
+                <p>
+                Cant get it out of my head,
+                <br/>
+because it was a vibe not experience.
+<br/>
+and aspirations of the vibe
+<br/>
+your vibe, the vibe you enjoy
+<br/>
+and cant get it outta your head
+                </p>
+                <br/>
+                <h6>16.</h6>
+                <br/>
+                <p>
+                I want to break free,
+                <br/>
+they couldnt let me.
+<br/>
+crabs pulling each other down
+<br/>
+reflecting one's fear upon another
+<br/>
+forcing one to come down
+<br/>
+onto their level
+<br/>
+binding in a cage
+<br/>
+of limited often single possibilities,
+                </p>
+                <br/>
+                <h6>17.</h6>
+                <br/>
+                <p>
+                lord grant me courage,
+                <br/>
+I can pretend to be fearlesss
+<br/>
+But cougrage isnt present
+<br/>
+don't ask courage to do what
+<br/>
+i wont have courage to say that
+<br/>
+bounded by my fearless fears
+<br/>
+and ridlled with the paradox of fears
+<br/>
+of death and life
+                </p>
+                <br/>
+                <h6>18</h6>
+                <br/>
+                <p>
+                common sense is not present,
+                <br/>
+when the eyes aren't damaged,
+<br/>
+but wisdom becomes blind
+<br/>
+without little damage to the eyes.
+                </p>
+                <br/><br/><br/>
+                <h6>19.</h6>
+                <br/>
+                <p>
+                Mirrors on the wall
+                <br/>
+grew up in it
+<br/>
+They say reflect,
+<br/>
+it does inevitably.
+<br/>
+respect the man,
+<br/>
+to look into the mirror.
+<br/>
+pat yourself on the back,
+<br/>
+if you can.
+<br/><br/>
+Lord grant the courage, 
+<br/>
+to those who shalt lack,
+<br/>
+tried to focus not in the eyes,
+<br/>
+but they stared back.
+<br/><br/>
+Stared back not onto me,
+<br/>
+but through me.
+<br/>
+Even i haven't seen that,
+<br/>
+I guess discovering new me.
+<br/>
+This new-me got courage,
+<br/>
+just like young one
+<br/>
+but, nah — i couldn't be that again,
+<br/>
+even if i tried. That fearlessness to look back
+<br/>
+into the mirror again.
+                </p>
             </div>
     </div>
 )
