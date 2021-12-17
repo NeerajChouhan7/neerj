d82f1a836234d87a4bc564f058eb5b1527c741c3
Neeraj Chouhan <48256974+NeerajChouhan7@users.noreply.github.com>
2021-12-17 16:04:41 +0530

    Update work.jsx

--- src/components/work.jsx
+++ src/components/work.jsx
@@ -20,8 +20,7 @@ function Work() {
             Writing newsletter on, about almost everything called <a href='https://neerajc.substack.com/'>Thinking Mold</a>.
            </li>
            <li>
-            Running a newsletter on psychology with ~300 members. <br/>
-            If you want to be part of it, click <a href='https://api.whatsapp.com/send/?phone=917247796922&text=Hey,+I+got+to+know+about+Psych.+I+would+love+to+be+part+of+it!&app_absent=0'>here</a> You can visit the archive <Link to='/psych-archives'>here</Link>
+            Running a newsletter on psychology with ~300 members. Click <a href='https://api.whatsapp.com/send/?phone=917247796922&text=Hey,+I+got+to+know+about+Psych.+I+would+love+to+be+part+of+it!&app_absent=0'>here</a> to join. You can visit the archives <Link to='/psych-archives'>here</Link> 
            </li>
            <br/>
            <h6>Did</h6>
@@ -29,19 +28,14 @@ function Work() {
             Wrote a research paper on Augmented Reality(AR) in Education. <a href='https://drive.google.com/file/d/1Dd4GnqjlP_PW39I23CVbKbgPTG4alB0i/view?usp=sharing' target='_blank' style={{borderRadius:'2px'}}><img class='link-img' src="https://img.icons8.com/ios-glyphs/30/000000/external-link.png"/></a> 
            </li>
            <li>
-             Product Management Intern at <a href='https://upraised.co/'>Upraised</a>
+             Product Management Intern at <a href='https://upraised.co/'>Upraised</a> (~10 months)
            </li>
            <li>
-             Front-end dev at <a href='https://www.revoteen.com/'>Revoteen</a>
+             Front-end dev at <a href='https://www.revoteen.com/'>Revoteen</a> (~4 months)
            </li>
-           <br />
-           <h6>Also...</h6>
            <li>
             Built several projects as a freelancer like chrome extension for Amazon sellers, E-commerce store to sell ppe-kits etc.
-           </li>
-           <li>
-            If you're Linkedin kinda...<a href='https://www.linkedin.com/in/neeraj-chouhan-profile/'>here</a>
-           </li>
+           </li>           
            <br/>
            <br/><br/>
         </div>
