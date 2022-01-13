29ec919497cfd2a96c4de29dd381aee06441573d
Neeraj Chouhan <48256974+NeerajChouhan7@users.noreply.github.com>
2022-01-13 15:30:28 +0530

    Create write.jsx

---
+++ src/components/all_blogs/write.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function write() {
+    return(
+        <div className='container mx-auto text-start mt-5'>
+                        <nav aria-label="breadcrumb">
+                <ol className="breadcrumb">
+                    <li className="breadcrumb-item"><Link to='/'>Home</Link></li>
+                    <li className="breadcrumb-item" aria-current="page"><Link to='/essays'>Essays</Link></li>
+                    <li className="breadcrumb-item active" aria-current="page">Why do I write?</li>
+                </ol>
+            </nav>
+            <div className='content-div'>
+            
+                <h1 tabindex="0" data-bs-toggle="popover" data-bs-placement="right" data-bs-trigger="hover focus" data-bs-content="20 Dec 2021" className='heading'>Why do I write?</h1>
+            
+          
+            <p><br />
+            <ol>
+                <li>
+                    <strong>To forget.</strong> I write to forget more than to remember. Incidents, people, emotion but especially ideas. I would go back to my writings sometimes and think. When and how did I even write that? Most ideas are evolving. But they evolve only after you have put them on paper. Because that version is on paper and now you can build a new, better version of it. Also, mind that I don’t write to forget intentionally. It’s something I have observed after writing for a short period of one year or so. When I go back to see my old writings, I forget that I had these ideas and perspectives. That I used to have this perspective toward this particular thing. It all starts to make sense once you have put them on paper and you see it in retrospect.
+                </li>
+                <li>
+
+<strong>To make sense.</strong> I am a teenager. We are biologically forced to feel new kinds of emotions every day. While it isn't necessary to make sense of everything I feel. I have a sin of intellectualizing my emotions. Even though that doesn't help me in being 'emotionally intelligent’. It feeds my curiosity. I still want to make sense of it, whether it helps or not. I find myself oscillating between thinking that emotions make you strong or weak.  I also observe changes in my behavior whenever I am on either side for a long time. Nonetheless, at this age, these emotions are just out of control. It’s better to make sense of at least some of them. In order to understand yourself, in being a rebel or not.
+                </li>
+                <li><strong> To rebel.</strong>Writing gives a sense of fake freedom. Being a rebel has always been my personality trait. And maybe writing being an unconventional thing fit into this trait of mine. Rebels have a big-time don’t-give-a-fuck mentality. Not in the outright rude way, but in a don’t-care-about-things-that-doesn’t-matter way. Also, one of the reasons why I dont give a single damn about what do I write here. From friendships and music to Defi, that is just my way of saying I don’t give a fuck to the world. You might have something else. That's the fake sense of freedom I was talking about earlier. Maybe to find a new you.</li>
+                <li><strong></strong></li>
+                <li><strong></strong></li>
+                <li><strong></strong></li>
+                <li><strong></strong></li>
+                <li><strong></strong></li>
+                <li><strong></strong></li>
+            </ol>
+
+
+
+
+4. To discover new ideas. Paul Graham says that one of the ingredients to be ‘beyond smart’ and discover new ideas is writing ability. To quote him:
+
+There's a class of new ideas that are best discovered by writing essays and books. And that "by" is deliberate: you don't think of the ideas first, and then merely write them down. There is a kind of thinking that one does by writing...
+
+Almost always I have found myself choosing a different route than my initial idea in writing a piece. Writing is constantly finding those sets of unique ideas. To think in ways you have never thought before. 5. To think. Only very limited people think. Those who can think are controlling the world. Also, the lives of people who don’t think. Sadly or not, that’s the way it is. Narrative by which the majority lives is controlled by a minority who can think. At its core, writing is thinking. It helps you create these narratives, which would have not been possible otherwise.
+
+6. To create. Writing is the only actual creative pursuit currently I do. And being an artistic person, you can't help but create. The way I can weave words together through my keyboard, having creases on my fingers by the bumps on F and J, to make a small place in your psyche about these utterly useless ideas feels fascinating to me. That’s the story I want to narrate.
+
+7. To narrate. The story we tell ourselves is paramount. Writing helps you talk to yourself. It lets you narrate your own story to yourself. Our inner reality is the story of our outer reality: the world. Writing helps you to clear your mind of the clutter that you had and lets you see through the substance. Only a handful of people can see their minds clearly. Writing is one of the tools to do that. Our stories are written by others and told to us when we were young. Most people just die with that story only. They don’t create their story. They don't create their own narratives. They are no less than zombies. But some people do. They don’t let the world decide for them who they should be. As Logan Paul says, “Can you remember who you were before the world told you who you should be?” Let that sink in. We are taught from a young age to do what everyone is doing. In one way or another. But to learn to live on your terms is a privilege only a few people have.
+
+8. To learn. Humans dont actually learn by consuming. You actually learn it when you can articulate what you consumed in your own version. We are coded to forget what we read. But we are more likely to remember our own version of it. Which is put into existence only by writing about it or teaching. You are more likely to remember by experience than by learning. Now think if we add a layer of writing over experiences. You can remember so much more, without even knowing.
+
+9. To remember. Contradicting to my very first point, Zadie Smith says that “the very reason I write is so that I might not sleepwalk through my entire life.” I have been journaling now for more than a year and I can vouch for that statement. I put in the effort there, just for me. I have written thousands of words in the night after having a good day with my friends. There is no incentive for me there. But the effort I put in, makes me relive those moments with my friends anytime I want. Not reliving like everyone else by old pictures on Instagram. But reliving by writing. Can't explain how beautiful that thought is for me. A friend of mine is leaving for college soon. That is a big change. I think people talk a lot about romantic breakups, but what about these breakups. I have known him for the last 4 years. He was there whenever I was having my best moments had some of my happiest moments with him. How can he just go? And that leaving is forever. Because there is no way he is going to come back as the same person he left. Or person he could have been if he was with me. I can't do anything about it and I should not. But I write about him to make me relive him just like I did when I had an amazing day with him. It helped me understand his situation because even I might do the same soon. And I should make sure to not have any regrets with any person before I leave.
+
+p.s. I was hesitant to put this piece out. Because this is a narcissistic post. It’s quite literally is. But then I read the title and 3rd point of this post again.
+
+p.p.s. anyways, if you got something new from it, comment and let me know. Even if it was as simple as a new thought that came into your mind when you were reading this. Let me know, ly :)
+</p>            
+<br />
+<br />
+<br/>
+            <p style={{color : "rgba(204, 238, 253, 0.884)"}}>Dec 2021</p>
+        </div>
+        </div>
+    )
+}
+ 
+export default write;
