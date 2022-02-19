b18f1c32afbd4b4e4a118b8a2fffeff6f0c5a827
Neeraj Chouhan <48256974+NeerajChouhan7@users.noreply.github.com>
2022-02-19 16:00:51 +0530

    Update love.jsx

--- src/components/all_blogs/love.jsx
+++ src/components/all_blogs/love.jsx
@@ -17,7 +17,7 @@ function love() {
             
           
             <p><br />
-            <div style={width= '100%'}>
+            <div>
                 <img src='https://cdn.substack.com/image/fetch/w_1456,c_limit,f_auto,q_auto:good,fl_progressive:steep/https%3A%2F%2Fbucketeer-e05bbc84-baa3-437e-9518-adb32be77984.s3.amazonaws.com%2Fpublic%2Fimages%2F90d36413-ac1a-4122-8ecf-701595a92740_750x1334.jpeg' />
             </div>
             This tree is right outside my house. I have more pictures of this tree than my own, as you will see down below. I certainly feel almost everything only after looking at this tree for a while. My mom thinks I am mad sometimes when I spend a lot of time just staring at it. One of the latest things that made me feel after looking at this tree was love. Not romance or all that Hollywood jazz, but just a feeling of love. The one where you feel one with something. It could be nature, a book, a song, your morning coffee, a tree, or anything. And well, sometimes, it’s another human being too. 
